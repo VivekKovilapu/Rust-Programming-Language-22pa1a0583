ada0016a07b6f808c2303494ad68ce7597bfc77f
Kovilapu Vivek <160890826+VivekKovilapu@users.noreply.github.com>
2025-06-25 10:24:16 +0530

    Create 2.rs

---
+++ 2.rs
@@ -0,0 +1,14 @@
+/*2. Write a program to display Output following pattern using Placeholders
+1
+22
+333
+4444
+55555
+*/
+fn main() {
+    println!("{}", "1".repeat(1));
+    println!("{}", "2".repeat(2));
+    println!("{}", "3".repeat(3));
+    println!("{}", "4".repeat(4));
+    println!("{}{}{}{}{}",5,5,5,5,5);
+}
